afaf8bd68b9070d85b13d4a403f7bf1f559fdb44
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2025-04-25 14:06:56 -0700

    add appdelegate to lock the screen to portrait. customize nav bar

---
+++ visual-acuity-test/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  visual-acuity-test
+//
+//  Created by Ryan Burns on 2025-04-25.
+//
+import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait
+    }
+}
--- visual-acuity-test/VisualAcuityTestApp.swift
+++ visual-acuity-test/VisualAcuityTestApp.swift
@@ -4,6 +4,17 @@ import SwiftUI
 
 @main
 struct visual_acuity_testApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    init() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backButtonAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.clear]
+        appearance.backButtonAppearance.highlighted.titleTextAttributes = [.foregroundColor: UIColor.clear]
+        UINavigationBar.appearance().tintColor = UIColor.systemOrange
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
